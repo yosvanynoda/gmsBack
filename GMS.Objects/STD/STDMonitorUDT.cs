17ed018737b38cfddb293af794a215c0bb633bf7
Mariela <mariela.ortiz.pa@gmail.com>
2025-12-09 20:49:23 -0500

    mobk-hotfix std

--- GMS.Objects/STD/STDMonitorUDT.cs
+++ GMS.Objects/STD/STDMonitorUDT.cs
@@ -4,6 +4,8 @@
     {
         public int STDId { get; set; } = 0;
 
+        public int Id { get; set; } = 0;
+
         public string FirstName { get; set; } = string.Empty;
 
         public string LastName { get; set; } = string.Empty;
